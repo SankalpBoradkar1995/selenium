fc75fdc2d4f6937905a014cdc332300b7da3b03b
Sankalp <boradkarsankalp6@gmail.com>
2023-11-19 21:36:26 +0530

    Added logic to execute function with js and getting SaleOfInfo details

--- src/main/java/com/web/selenium/PrivacySettings.java
+++ src/main/java/com/web/selenium/PrivacySettings.java
@@ -18,88 +18,114 @@ import org.testng.annotations.Test;
 
 public class PrivacySettings {
 
-    private String url = "https://www.verizon.com/";
-    private WebDriver driver;
-    private WebDriverWait wait;
-    private String elemntOnLoading = "gnav20-Shop-L1";
-    private String privacyLink = "//*[contains(text(), 'Your Privacy Choices')][1]";
-    private String onLoadingPrivacyPage = "gnav20-Shop-L1";
-    private String optOutId = "optOut";
-    private String optInId = "doNotOptOut";
-    private String privacyDialogBox = ".ModalDialog-sc-1m9nirl-0[role='dialog']";
-    private String privacyDialogBoxYes = "button[aria-label='Yes']";
+	private String url = "https://www.verizon.com/";
+	private WebDriver driver;
+	private WebDriverWait wait;
+	//on page loading / redirect will wait for this element to be visible before doing any operation. Just to avoid thread.sleep
+	private String elemntOnLoading = "gnav20-Shop-L1";
+	private String privacyLink = "//*[contains(text(), 'Your Privacy Choices')][1]";
+//	private String onLoadingPrivacyPage = "gnav20-Shop-L1";
+	
+	// These are two radio buttons opt in / opt out
+	private String optOutId = "optOut";
+	private String optInId = "doNotOptOut";
+	
+	// This is the dialog box on clicking yes button
+	private String privacyDialogBox = ".ModalDialog-sc-1m9nirl-0[role='dialog']";
+	
+	// This is the yes button option for dialog
+	private String privacyDialogBoxYes = "button[aria-label='Yes']";
 
-    @BeforeTest
-    public void init() {
-        System.setProperty("webdriver.chrome.driver",
-                System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe");
-        
-        ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("w3c", false);
-        options.addArguments("--remote-allow-origins=*");
-        
-        options.setCapability("goog:loggingPrefs", "{\"browser\": \"ALL\"}");
+	@BeforeTest
+	public void init() {
 
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-    }
+		
+		// Setting up the driver
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe");
 
-    @Test
-    public void clickOptIn() throws InterruptedException {
-        driver.get(url);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(elemntOnLoading)));
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("w3c", false);
+		options.addArguments("--remote-allow-origins=*");
 
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
-                driver.findElement(By.xpath(privacyLink)));
-        driver.findElement(By.xpath(privacyLink)).click();
+		options.setCapability("goog:loggingPrefs", "{\"browser\": \"ALL\"}");
 
-        WebElement optIn = driver.findElement(By.id(optInId));
+		driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	}
 
-        if (!optIn.isSelected()) {
-            optIn.click();
-            handleDialog();
-        }
-        LogEntries logs = driver.manage().logs().get("browser");
-        for (LogEntry entry : logs) {
-            System.out.println(entry.getMessage());
-        }
-    }
+	@Test
+	public void clickOptIn() throws InterruptedException {
+		driver.get(url);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(elemntOnLoading)));
 
-    @Test
-    public void clickOptOut() {
-        driver.get(url);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(elemntOnLoading)));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
-                driver.findElement(By.xpath(privacyLink)));
-        driver.findElement(By.xpath(privacyLink)).click();
-        WebElement optOut = driver.findElement(By.id(optOutId));
-        optOut.click();
-    }
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
+				driver.findElement(By.xpath(privacyLink)));
+		driver.findElement(By.xpath(privacyLink)).click();
 
-    private void handleDialog() {
-    	 try {
-             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-             WebElement dialogBox = wait.until(
-                     ExpectedConditions.visibilityOfElementLocated(By.cssSelector(privacyDialogBox)));
-             WebElement okButton = dialogBox.findElement(By.cssSelector(privacyDialogBoxYes));
-             okButton.click();
-             driver.switchTo().defaultContent();
-             wait.until(ExpectedConditions
-                     .invisibilityOfElementLocated(By.cssSelector(privacyDialogBox)));
-         } catch (Exception e) {
-             e.printStackTrace();
-         }
-    }
-    
-    
+		WebElement optIn = driver.findElement(By.id(optInId));
 
-    @AfterTest
-    public void shutDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+		if (!optIn.isSelected()) {
+			optIn.click();
+			handleDialog();
+		}
+		LogEntries logs = driver.manage().logs().get("browser");
+//		for (LogEntry entry : logs) {
+//			System.out.println(entry.getMessage());
+//		}
+		// Create an instance of JavascriptExecutor
+		JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+
+		// Execute the airgap.getConsent() command and store the result
+		Object result = jsExecutor.executeScript("return airgap.getConsent();");
+
+		boolean saleOfInfoValue = (boolean) ((org.openqa.selenium.JavascriptExecutor) jsExecutor)
+				.executeScript("return arguments[0].purposes.SaleOfInfo;", result);
+
+		System.out.println("SaleOfInfo status: " + saleOfInfoValue);
+		// Print the result
+		System.out.println("Consent Result: " + result);
+	}
+
+	@Test
+	public void clickOptOut() {
+		driver.get(url);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(elemntOnLoading)));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
+				driver.findElement(By.xpath(privacyLink)));
+		driver.findElement(By.xpath(privacyLink)).click();
+		WebElement optOut = driver.findElement(By.id(optOutId));
+		optOut.click();
+		JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+		Object result = jsExecutor.executeScript("return airgap.getConsent();");
+		boolean saleOfInfoValue = (boolean) ((org.openqa.selenium.JavascriptExecutor) jsExecutor)
+				.executeScript("return arguments[0].purposes.SaleOfInfo;", result);
+		System.out.println("SaleOfInfo status: " + saleOfInfoValue);
+		// Print the result
+		System.out.println("Consent Result: " + result);
+	}
+
+	private void handleDialog() {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			WebElement dialogBox = wait
+					.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(privacyDialogBox)));
+			WebElement okButton = dialogBox.findElement(By.cssSelector(privacyDialogBoxYes));
+			okButton.click();
+			driver.switchTo().defaultContent();
+			wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(privacyDialogBox)));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@AfterTest
+	public void shutDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 }
--- src/main/java/com/web/selenium/test.java
+++ src/main/java/com/web/selenium/test.java
@@ -14,81 +14,126 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class test {
-	  private static String url = "https://www.verizon.com/";
-	    private static WebDriver driver;
-	    private static WebDriverWait wait;
-	    private static String elemntOnLoading = "gnav20-Shop-L1";
-	    private static String privacyLink = "//*[contains(text(), 'Your Privacy Choices')][1]";
-	    private static String onLoadingPrivacyPage = "gnav20-Shop-L1";
-	    private static String optOutId = "optOut";
-	    private static String optInId = "doNotOptOut";
-	    private static String privacyDialogBox = ".ModalDialog-sc-1m9nirl-0[role='dialog']";
-	    private static String privacyDialogBoxYes = "button[aria-label='Yes']";
+	private static String url = "https://www.verizon.com/";
+
+	private static WebDriver driver;
+	private static WebDriverWait wait;
+	// on page loading / redirect will wait for this element to be visible before
+	// doing any operation. Just to avoid thread.sleep
+	private static String elemntOnLoading = "gnav20-Shop-L1";
+	private static String privacyLink = "//*[contains(text(), 'Your Privacy Choices')][1]";
+//		private String onLoadingPrivacyPage = "gnav20-Shop-L1";
+
+	// These are two radio buttons opt in / opt out
+	private static String optOutId = "optOut";
+	private static String optInId = "doNotOptOut";
+
+	// This is the dialog box on clicking yes button
+	private static String privacyDialogBox = ".ModalDialog-sc-1m9nirl-0[role='dialog']";
+
+	// This is the yes button option for dialog
+	private static String privacyDialogBoxYes = "button[aria-label='Yes']";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// Here configuring the driver
+
 		System.setProperty("webdriver.chrome.driver",
-                System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe");
-        
-        ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("w3c", false);
-        options.addArguments("--remote-allow-origins=*");
-        
-        options.setCapability("goog:loggingPrefs", "{\"browser\": \"ALL\"}");
+				System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe");
 
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.get(url);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(elemntOnLoading)));
+		// getting 403 sometimes while accing the url so added chrome options
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("w3c", false);
+		options.addArguments("--remote-allow-origins=*");
 
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
-                driver.findElement(By.xpath(privacyLink)));
-        driver.findElement(By.xpath(privacyLink)).click();
+		// options.setCapability("goog:loggingPrefs", "{\"browser\": \"ALL\"}");
 
-        WebElement optIn = driver.findElement(By.id(optInId));
+		driver = new ChromeDriver(options);
 
-        if (!optIn.isSelected()) {
-            optIn.click();
-            handleDialog();
-        }
-        LogEntries logs = driver.manage().logs().get("browser");
-        for (LogEntry entry : logs) {
-            System.out.println(entry.getMessage());
-        }
-        
-        driver.get(url);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(elemntOnLoading)));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
-                driver.findElement(By.xpath(privacyLink)));
-        driver.findElement(By.xpath(privacyLink)).click();
-        WebElement optOut = driver.findElement(By.id(optOutId));
-        optOut.click();
-        
-        if(driver!=null)
-        {
-        	driver.quit();
-        }
+		// maximazing the window
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+
+		// Adding wait to look elements to load instead of adding thread.sleep
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		
+		//------------------
+		// This flow is for opt in button
+		driver.get(url);
+
+		// Waiting for element to load when passing the url to driver
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(elemntOnLoading)));
+
+		// Scrolling to bottom of the page to fing privacy setting option at footer and
+		// clicking it
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
+				driver.findElement(By.xpath(privacyLink)));
+		driver.findElement(By.xpath(privacyLink)).click();
+
+		// On your privacy settings page locating the radio button "Opt in"
+
+		WebElement optIn = driver.findElement(By.id(optInId));
+
+		// Checking if the "Opt In" button is selected or not, if not selecting it and
+		// then calling the handleDialog function to acccept it
+		if (!optIn.isSelected()) {
+			optIn.click();
+			handleDialog();
+		}
+
+		// After clicking yes in dialog box, executing the command
+		// Create an instance of JavascriptExecutor
+		JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+
+		// Execute the airgap.getConsent() command and store the result
+		Object result = jsExecutor.executeScript("return airgap.getConsent();");
+		
+		boolean saleOfInfoValue = (boolean) ((org.openqa.selenium.JavascriptExecutor) jsExecutor)
+				.executeScript("return arguments[0].purposes.SaleOfInfo;", result);
+
+		System.out.println("SaleOfInfo status: " + saleOfInfoValue);
+		// Print the result
+		System.out.println("Consent Result: " + result);
+		
+		//-------------------
+		// This flow is for opt out button
+		
+		driver.get(url);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(elemntOnLoading)));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
+				driver.findElement(By.xpath(privacyLink)));
+		driver.findElement(By.xpath(privacyLink)).click();
+		WebElement optOut = driver.findElement(By.id(optOutId));
+		optOut.click();
+		result = jsExecutor.executeScript("return airgap.getConsent();");
+		saleOfInfoValue = (boolean) ((org.openqa.selenium.JavascriptExecutor) jsExecutor)
+				.executeScript("return arguments[0].purposes.SaleOfInfo;", result);
+		System.out.println("SaleOfInfo status: " + saleOfInfoValue);
+		// Print the result
+		System.out.println("Consent Result: " + result);
+		
+		// After the flow terminating the driver instance 
+
+		if (driver != null) {
+			driver.quit();
+		}
 
 	}
 
 	private static void handleDialog() {
-		 try {
-             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-             WebElement dialogBox = wait.until(
-                     ExpectedConditions.visibilityOfElementLocated(By.cssSelector(privacyDialogBox)));
-             WebElement okButton = dialogBox.findElement(By.cssSelector(privacyDialogBoxYes));
-             okButton.click();
-             driver.switchTo().defaultContent();
-             wait.until(ExpectedConditions
-                     .invisibilityOfElementLocated(By.cssSelector(privacyDialogBox)));
-         } catch (Exception e) {
-             e.printStackTrace();
-         }
-		
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			WebElement dialogBox = wait
+					.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(privacyDialogBox)));
+			WebElement okButton = dialogBox.findElement(By.cssSelector(privacyDialogBoxYes));
+			okButton.click();
+			driver.switchTo().defaultContent();
+			wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(privacyDialogBox)));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
