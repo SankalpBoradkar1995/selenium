13deb3dcb5ecdc9fcdadad25b84c1ac3b4c32bd2
Sankalp <boradkarsankalp6@gmail.com>
2023-11-20 23:19:02 +0530

    Added separate class for Opt In privacy mode

---
+++ src/main/java/com/web/selenium/PrivacyOptIn.java
@@ -0,0 +1,137 @@
+package com.web.selenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+public class PrivacyOptIn {
+	private static String url = "https://www.verizon.com/digital/nsa/nos/ui/acct/privacy/your-privacy-choices";
+	private static WebDriver driver;
+	private static WebDriverWait wait;
+	// on page loading / redirect will wait for this element to be visible before
+	// doing any operation. Just to avoid thread.sleep
+	private String elemntOnLoading = "gnav20-Shop-L1";
+
+	// These are two radio buttons opt in / opt out
+
+	private static String optInId = "doNotOptOut";
+
+	// This is the dialog box on clicking yes button
+	private static String privacyDialogBox = ".ModalDialog-sc-1m9nirl-0[role='dialog']";
+
+	// This is the yes button option for dialog
+	private static String privacyDialogBoxYes = "button[aria-label='Yes']";
+
+	// Shop button
+	private static String shopButtonId = "gnav20-Shop-L1";
+
+	// Devices option
+	private static String devicesId = "gnav20-Shop-L2-3";
+
+	// Smartphone option
+	private static String smartPhoneId = "gnav20-Shop-L3-1";
+
+	// Help me choices pop up
+	private static String helpMeChoicesId = "_15gifts-popup-wrapper-testId";
+
+	// Chat close button
+	private static String closeChat = "//*[@data-lp-event='close']";
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe");
+
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("w3c", false);
+		options.addArguments("--remote-allow-origins=*");
+
+		driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		driver.get(url);
+		WebElement optIn = driver.findElement(By.id(optInId));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
+				driver.findElement(By.id(optInId)));
+		wait.until(ExpectedConditions.elementToBeClickable(optIn));
+
+		if (!optIn.isSelected()) {
+			optIn.click();
+			handleDialog();
+		}
+
+		// Clicking Shop button
+		driver.findElement(By.id(shopButtonId)).click();
+
+		// Waiting for Devices button to be clickable then clicking it
+		wait.until(ExpectedConditions.elementToBeClickable(By.id(devicesId)));
+		driver.findElement(By.id(devicesId)).click();
+
+		// Waiting for Smartphone button to be clickable then clicking it
+		wait.until(ExpectedConditions.elementToBeClickable(By.id(smartPhoneId)));
+		driver.findElement(By.id(smartPhoneId)).click();
+
+		// Scrolling and selecting first phone option available
+		driver.findElement(By.cssSelector("#MTQP3LL\\/A-null > #productDetails img")).click();
+		// Selecting phone color
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".swatch:nth-child(2) > .h-9")));
+		driver.findElement(By.cssSelector(".swatch:nth-child(2) > .h-9")).click();
+
+		// *[@id="128"]/div/label
+		// Scrolling down to selet storage
+		Thread.sleep(3000);
+		JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+
+		// Execute the airgap.getConsent() command and store the result
+		Object result = jsExecutor.executeScript("return airgap.getConsent();");
+		System.out.println("Consent Result: " + result);
+		boolean saleOfInfoValue;
+		String OptInString;
+		try {
+			saleOfInfoValue = (boolean) ((org.openqa.selenium.JavascriptExecutor) jsExecutor)
+					.executeScript("return arguments[0].purposes.SaleOfInfo;", result);
+			System.out.println("SaleOfInfo status: " + saleOfInfoValue);
+		} catch (ClassCastException e) {
+			if (e.getMessage().contains("class java.lang.String cannot be cast to class java.lang.Boolean")) {
+				OptInString = (String) ((org.openqa.selenium.JavascriptExecutor) jsExecutor)
+						.executeScript("return arguments[0].purposes.SaleOfInfo;", result);
+				System.out.println("SaleOfInfo status: " + OptInString);
+				if (!(OptInString.equalsIgnoreCase("Auto") || OptInString.equalsIgnoreCase("true"))) {
+					Assert.assertTrue(true, "Sale of info is false if opting in");
+				}
+			}
+		}
+		
+		if(driver!=null)
+		{
+			driver.quit();
+		}
+
+	}
+
+	private static void handleDialog() {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			WebElement dialogBox = wait
+					.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(privacyDialogBox)));
+			WebElement okButton = dialogBox.findElement(By.cssSelector(privacyDialogBoxYes));
+			okButton.click();
+			driver.switchTo().defaultContent();
+			wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(privacyDialogBox)));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
--- src/main/java/com/web/selenium/PrivacySettings.java
+++ src/main/java/com/web/selenium/PrivacySettings.java
@@ -1,6 +1,8 @@
 package com.web.selenium;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -8,17 +10,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.logging.LogEntries;
-import org.openqa.selenium.logging.LogEntry;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class PrivacySettings {
 
-	private String url = "https://www.verizon.com/";
+	//private String url = "https://www.verizon.com/";
+	private String url = "https://www.verizon.com/digital/nsa/nos/ui/acct/privacy/your-privacy-choices";
 	private WebDriver driver;
 	private WebDriverWait wait;
 	//on page loading / redirect will wait for this element to be visible before doing any operation. Just to avoid thread.sleep
@@ -35,6 +39,21 @@ public class PrivacySettings {
 	
 	// This is the yes button option for dialog
 	private String privacyDialogBoxYes = "button[aria-label='Yes']";
+	
+	// Shop button
+	private String shopButtonId = "gnav20-Shop-L1";
+	
+	// Devices option
+	private String devicesId = "gnav20-Shop-L2-3";
+	
+	// Smartphone option
+	private String smartPhoneId = "gnav20-Shop-L3-1";
+	
+	// Help me choices pop up
+	private String helpMeChoicesId = "_15gifts-popup-wrapper-testId";
+	
+	// Chat close button
+	private String closeChat = "//*[@data-lp-event='close']";
 
 	@BeforeTest
 	public void init() {
@@ -57,16 +76,19 @@ public class PrivacySettings {
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	}
 
-	@Test
+	@Test(enabled=true)
 	public void clickOptIn() throws InterruptedException {
 		driver.get(url);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(elemntOnLoading)));
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(elemntOnLoading)));
 
-		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
-				driver.findElement(By.xpath(privacyLink)));
-		driver.findElement(By.xpath(privacyLink)).click();
+//		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
+//				driver.findElement(By.xpath(privacyLink)));
+//		driver.findElement(By.xpath(privacyLink)).click();
 
 		WebElement optIn = driver.findElement(By.id(optInId));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
+				driver.findElement(By.id(optInId)));
+		wait.until(ExpectedConditions.elementToBeClickable(optIn));
 
 		if (!optIn.isSelected()) {
 			optIn.click();
@@ -81,21 +103,39 @@ public class PrivacySettings {
 
 		// Execute the airgap.getConsent() command and store the result
 		Object result = jsExecutor.executeScript("return airgap.getConsent();");
-
-		boolean saleOfInfoValue = (boolean) ((org.openqa.selenium.JavascriptExecutor) jsExecutor)
-				.executeScript("return arguments[0].purposes.SaleOfInfo;", result);
-
-		System.out.println("SaleOfInfo status: " + saleOfInfoValue);
-		// Print the result
 		System.out.println("Consent Result: " + result);
+
+		boolean saleOfInfoValue;
+		String OptInString;
+		try {
+			saleOfInfoValue = (boolean) ((org.openqa.selenium.JavascriptExecutor) jsExecutor)
+					.executeScript("return arguments[0].purposes.SaleOfInfo;", result);
+			System.out.println("SaleOfInfo status: " + saleOfInfoValue);
+		} catch (ClassCastException e) {
+			if(e.getMessage().contains("class java.lang.String cannot be cast to class java.lang.Boolean"))
+			{
+				OptInString = (String) ((org.openqa.selenium.JavascriptExecutor) jsExecutor)
+						.executeScript("return arguments[0].purposes.SaleOfInfo;", result);
+				System.out.println("SaleOfInfo status: "+OptInString);
+				if(!(OptInString.equalsIgnoreCase("Auto")||OptInString.equalsIgnoreCase("true")))
+				{
+					Assert.assertTrue(true,"Sale of info is false if opting in");
+				}
+			}
+		}
+
+		
+		// Print the result
+		
 	}
 
-	@Test
-	public void clickOptOut() {
+	@Test(enabled=false)
+	public void clickOptOut() throws InterruptedException {
 		driver.get(url);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(elemntOnLoading)));
 		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
-				driver.findElement(By.xpath(privacyLink)));
+				driver.findElement(By.id(optOutId)));
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(optOutId)));
 		driver.findElement(By.xpath(privacyLink)).click();
 		WebElement optOut = driver.findElement(By.id(optOutId));
 		optOut.click();
@@ -106,6 +146,74 @@ public class PrivacySettings {
 		System.out.println("SaleOfInfo status: " + saleOfInfoValue);
 		// Print the result
 		System.out.println("Consent Result: " + result);
+		
+		// Clicking Shop button
+		driver.findElement(By.id(shopButtonId)).click();
+		
+		// Waiting for Devices button to be clickable then clicking it
+		wait.until(ExpectedConditions.elementToBeClickable(By.id(devicesId)));
+		driver.findElement(By.id(devicesId)).click();
+		
+		// Waiting for Smartphone button to be clickable then clicking it
+		wait.until(ExpectedConditions.elementToBeClickable(By.id(smartPhoneId)));
+		driver.findElement(By.id(smartPhoneId)).click();
+		
+		// Closing chat card
+//		WebElement chatOption = driver.findElement(By.xpath(closeChat));
+//		wait.until(ExpectedConditions.visibilityOf(chatOption));
+//		chatOption.click();
+		
+		// Find all product options using the XPath
+//        List<WebElement> productOptions = driver.findElements(By.xpath("//div[contains(@class, 'grid') and contains(@class, 'gap-3') and contains(@class, 'w-full') and contains(@class, 'grid-cols-2')]/div"));
+//        
+//     // Scroll to a random product from the list above and click it
+//        if(!productOptions.isEmpty())
+//        {
+//        	 // Get a random index
+//            int randomIndex = new Random().nextInt(productOptions.size());
+//         // Scroll to the random product
+//            WebElement randomProduct = productOptions.get(randomIndex);
+//            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", randomProduct);
+//            wait.until(ExpectedConditions.visibilityOf(randomProduct));
+//            wait.until(ExpectedConditions.elementToBeClickable(randomProduct));
+//            // Click the random product
+//            Actions action = new Actions(driver);
+//            action.moveToElement(randomProduct).build().perform();
+//            action.click(randomProduct).build().perform();
+//            randomProduct.click();
+//           // randomProduct.click();
+//            Thread.sleep(5000);
+//        }
+//        else
+//        {
+//        	 System.out.println("No product options found.");
+//        }
+		
+		// Scrolling and selecting first phone option available 
+		driver.findElement(By.cssSelector("#MTQP3LL\\/A-null > #productDetails img")).click();
+		// Selecting phone color
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".swatch:nth-child(2) > .h-9")));
+		driver.findElement(By.cssSelector(".swatch:nth-child(2) > .h-9")).click();
+		
+		//*[@id="128"]/div/label
+		// Scrolling down to selet storage
+		Thread.sleep(3000);
+		jsExecutor.executeScript("window.scrollTo(0,12)");
+		// selecting storage
+		driver.findElement(By.id("label-clp7391t3000g358jb7vgbjq3")).click();
+		//driver.findElement(By.id("//*[@id='128']/div/label")).click();
+		// Selecting New Customer button
+		driver.findElement(By.cssSelector("#label-clp7393vu000o358jsc63nrme > .ChildWrapper-VDS__sc-n9jxr1-2 > div > div > .flex")).click();
+		// Scrolling down
+		jsExecutor.executeScript("window.scrollTo(0,478)");
+		
+		WebElement element = driver.findElement(By.cssSelector(".glVjuF > .HitArea-VDS__sc-bc3yhn-0"));
+	    Actions builder = new Actions(driver);
+	    builder.moveToElement(element).perform();
+		  
+		Thread.sleep(5000);
+
+		
 	}
 
 	private void handleDialog() {
@@ -124,8 +232,8 @@ public class PrivacySettings {
 
 	@AfterTest
 	public void shutDown() {
-		if (driver != null) {
-			driver.quit();
-		}
+//		if (driver != null) {
+//			driver.quit();
+//		}
 	}
 }
